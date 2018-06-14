94621ec64236b03076ce16a74295a9c0ded65c46
GGKTECH\prabhuji.mudragalla <prabhuji.mudragalla@gmail.com>
2018-06-14 10:29:12 +0530

    Changse for Swagger

--- JSP.Volunteer/Startup.cs
+++ JSP.Volunteer/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using JSP.Data.Interfaces;
 using JSP.Data.Context;
 using JSP.Data.Model;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace JSP.Volunteer
 {
@@ -31,6 +32,12 @@ namespace JSP.Volunteer
 
             services.AddMvc();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Volunteer API", Version = "v1" });
+            });
+
             services.Configure<Settings>(options =>
             {
                 options.ConnectionString = Configuration.GetSection("MongoConnection:ConnectionString").Value;
@@ -44,12 +51,24 @@ namespace JSP.Volunteer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
+
             app.UseCors("CorsPolicy");
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Volunteer API V1");
+            });
 
             app.UseMvc();
         }
